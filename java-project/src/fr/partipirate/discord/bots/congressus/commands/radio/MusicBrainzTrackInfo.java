bbb5e900f26388338d35f91640525e525a6e017e
Xeladaren <alexandre.cr.basque@gmail.com>
2019-12-22 16:41:06 +0100

    EmbedBuilder to !nowplaying V2 ( get cover on MusicBrainz )

---
+++ java-project/src/fr/partipirate/discord/bots/congressus/commands/radio/MusicBrainzTrackInfo.java
@@ -0,0 +1,114 @@
+package fr.partipirate.discord.bots.congressus.commands.radio;
+
+public class MusicBrainzTrackInfo {
+
+	private String recordingName, recordingID, recordingURL ;
+
+	private String artistName, artistID, artistURL ;
+
+	private String releaseName, releaseID ;
+
+	private String coverURL ;
+
+	// *** SETTERS
+
+	// ** Recording setters **
+
+	// Recording name setter
+	public void setRecordingName(String value){
+		this.recordingName = value ;
+	}
+
+	// Recording ID setter
+	public void setRecordingID(String value){
+		this.recordingID = value ;
+	}
+
+	// Recording URL setter
+	public void setRecordingURL(String value){
+		this.recordingURL = value ;
+	}
+
+	// ** Artist setters **
+
+	// Artist name setter
+	public void setArtistName(String value){
+		this.artistName = value ;
+	}
+
+	// Artist ID setter
+	public void setArtistID(String value){
+		this.artistID = value ;
+	}
+
+	// Artist URL setter
+	public void setArtistURL(String value){
+		this.artistURL = value ;
+	}
+
+	// ** Release setters **
+
+	// Release name setter
+	public void setReleaseName(String value){
+		this.releaseName = value ;
+	}
+
+	// Release ID setter
+	public void setReleaseID(String value){
+		this.releaseID = value ;
+	}
+
+	// ** Cover setter **
+
+	public void setCoverURL(String value){
+		this.coverURL = value ;
+	}
+
+	// *** GETTERS ***
+
+	// Recording ID getter
+	public String getRecordingID(){
+		return this.recordingID ;
+	}
+
+	// Recording URL getter
+	public String getRecordingURL(){
+		return this.recordingURL ;
+	}
+
+	// ** Artist getter **
+
+	// Artist name getter
+	public String getArtistName(){
+		return this.artistName ;
+	}
+
+	// Artist ID getter
+	public String getArtistID(){
+		return this.artistID ;
+	}
+
+	// Artist URL getter
+	public String getArtistURL(){
+		return this.artistURL ;
+	}
+
+	// ** Release getter **
+
+	// Release name getter
+	public String getReleaseName(){
+		return this.releaseName ;
+	}
+
+	// Release ID getter
+	public String getReleaseID(){
+		return this.releaseID ;
+	}
+
+	// ** Cover getter **
+
+	public String getCoverURL(){
+		return this.coverURL ;
+	}
+
+}
--- java-project/src/fr/partipirate/discord/bots/congressus/commands/radio/RadioHelper.java
+++ java-project/src/fr/partipirate/discord/bots/congressus/commands/radio/RadioHelper.java
@@ -9,13 +9,16 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Iterator;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.JSONTokener;
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
 import fr.partipirate.discord.bots.congressus.Configuration;
+import fr.partipirate.discord.bots.congressus.commands.radio.MusicBrainzTrackInfo;
 
 public class RadioHelper {
 
@@ -118,6 +121,180 @@ public class RadioHelper {
 		}
 
 		return true;
+	}
+
+	public static MusicBrainzTrackInfo searchTrackInfo(String trackName, String artistName){
+
+		MusicBrainzTrackInfo mbTrackInfo = new MusicBrainzTrackInfo() ;
+
+		String searchQuery = trackName ;
+
+		searchQuery.toLowerCase​() ;
+		searchQuery.replaceAll​(" ", "+") ;
+		searchQuery.replaceAll​("&", "%26") ;
+
+		String searchURL = "https://musicbrainz.org/ws/2/recording?query=" + searchQuery + "&fmt=json" ;
+
+		try {
+
+			JSONObject musicBrainsReply = call(searchURL) ;
+
+			if (musicBrainsReply.has("recordings")) {
+
+				JSONArray recordingsArray = musicBrainsReply.getJSONArray("recordings") ;
+
+				boolean isFind = false ;
+				int i = 0 ;
+
+
+				// Search artist name in the recording list.
+				while( !isFind && i < recordingsArray.length()){
+
+					if (recordingsArray.getJSONObject(i).has("artist-credit")) {
+						
+						JSONArray artistArray = recordingsArray.getJSONObject(i).getJSONArray("artist-credit") ;
+
+						boolean haveArtist = false ;
+						int j = 0 ;
+
+						// Search artist name in artists list
+						while(!haveArtist && j < artistArray.length()){
+
+							JSONObject artistObject = artistArray.getJSONObject(i) ;
+
+							if (artistObject.has("name") && artistObject.getString("name").equalsIgnoreCase​(artistName)) {
+							 		
+							 		isFind = true ;
+
+							 		if (artistObject.has("artist") && artistObject.getJSONObject("artist").has("id")) {
+
+							 			mbTrackInfo.setArtistID(artistObject.getJSONObject("artist").getString("id")) ;
+
+							 			mbTrackInfo.setArtistName(artistObject.getString("name")) ;
+
+							 			String artistURL = "https://musicbrainz.org/artist/" + mbTrackInfo.getArtistID() ;
+
+							 			mbTrackInfo.setArtistURL(artistURL) ;
+
+							 		}
+
+							}
+
+							j++ ; 
+						}
+
+						if (haveArtist) {
+
+							if (recordingsArray.getJSONObject(i).has("releases")) {
+								JSONArray releasesArray = recordingsArray.getJSONObject(i).getJSONArray("releases") ;
+
+								if (releasesArray.length() > 0) {
+									
+									JSONObject releaseObject = releasesArray.getJSONObject(0) ;
+
+									if (releaseObject.has("id")) {
+										mbTrackInfo.setReleaseID(releaseObject.getString("id")) ;
+									}
+
+									if (releaseObject.has("title")) {
+										mbTrackInfo.setReleaseName(releaseObject.getString("title")) ;
+										
+									}
+
+								}
+							}
+
+							if (recordingsArray.getJSONObject(i).has("id")) {
+
+								mbTrackInfo.setRecordingID(recordingsArray.getJSONObject(i).getString("id")) ;
+
+								String artistURL = "https://musicbrainz.org/recording/" + mbTrackInfo.getArtistID() ;
+
+							 	mbTrackInfo.setRecordingURL(artistURL) ;
+
+							}
+
+							if (recordingsArray.getJSONObject(i).has("title")) {
+
+								mbTrackInfo.setRecordingName(recordingsArray.getJSONObject(i).getString("title")) ;
+
+							}
+
+							String coverURL = getCoverURLInCoverArtArchive(mbTrackInfo.getReleaseID()) ;
+
+							mbTrackInfo.setCoverURL(coverURL) ;
+
+							return mbTrackInfo ;
+							
+						}
+
+					}
+
+
+					i++ ;
+				}
+
+			}
+
+		}
+		catch (Exception e) {
+		}
+
+		return null ;
+
+	}
+
+	private static String getCoverURLInCoverArtArchive(String id){
+
+		String searchURL = "https://ia801900.us.archive.org/13/items/mbid-" + id + "/index.json" ;
+
+		try {
+
+			JSONObject coverArchiveReply = call(searchURL) ;
+
+			if (coverArchiveReply.has("images")) {
+
+				JSONArray imagesArray = coverArchiveReply.getJSONArray("images") ;
+
+				if (imagesArray.length() > 0) {
+
+					JSONObject imageObject = imagesArray.getJSONObject(0) ;
+
+					if (imageObject.has("thumbnails")) {
+
+						if (imageObject.getJSONObject("thumbnails").has("small")) {
+
+							return imageObject.getJSONObject("thumbnails").getString("small") ;
+
+						}
+						else{
+
+							Iterator<String> keyList = imageObject.getJSONObject("thumbnails").keys() ;
+
+							if (keyList.hasNext()) {
+
+								return imageObject.getJSONObject("thumbnails").getString(keyList.next()) ;
+								
+							}
+
+						}
+						
+					}
+					if (imageObject.has("image")) {
+
+						return imageObject.getString("image") ;
+
+					}
+					
+				}
+				
+			}
+
+		}
+		catch (Exception e) {
+		}
+
+		return null ;
 	}
 
 	private static JSONObject call(String apiCallUrl) throws IOException {
