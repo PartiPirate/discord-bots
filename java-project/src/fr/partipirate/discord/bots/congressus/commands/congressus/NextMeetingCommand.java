9cfc13080df01e324c9846a8e4ecf6f00e26512e
Cédric Levieux <contact@levieuxcedric.com>
2020-02-15 22:39:43 +0100

    unescape

--- java-project/src/fr/partipirate/discord/bots/congressus/commands/congressus/NextMeetingCommand.java
+++ java-project/src/fr/partipirate/discord/bots/congressus/commands/congressus/NextMeetingCommand.java
@@ -131,11 +131,11 @@ public class NextMeetingCommand extends ACongressusCommand implements ICommand {
 				eb.setColor(new Color(0xF40C0C));
 				
 //				eb.setDescription("Text");
-				eb.addField("Horaire", StringEscapeUtils.escapeHtml4(minMeeting.getString("meetingDatetime")), false);
-				eb.addField("Lieu", StringEscapeUtils.escapeHtml4(minMeeting.getJSONObject("location").getString("type")), false);
+				eb.addField("Horaire", StringEscapeUtils.unescapeHtml4(minMeeting.getString("meetingDatetime")), false);
+				eb.addField("Lieu", StringEscapeUtils.unescapeHtml4(minMeeting.getJSONObject("location").getString("type")), false);
 				if (minMeeting.getJSONObject("location").has("discord")) {
-					eb.addField(":hash:", StringEscapeUtils.escapeHtml4(minMeeting.getJSONObject("location").getJSONObject("discord").getJSONObject("text").getString("title")), true);
-					eb.addField(":sound:", StringEscapeUtils.escapeHtml4(minMeeting.getJSONObject("location").getJSONObject("discord").getJSONObject("vocal").getString("title")), true);
+					eb.addField(":hash:", StringEscapeUtils.unescapeHtml4(minMeeting.getJSONObject("location").getJSONObject("discord").getJSONObject("text").getString("title")), true);
+					eb.addField(":sound:", StringEscapeUtils.unescapeHtml4(minMeeting.getJSONObject("location").getJSONObject("discord").getJSONObject("vocal").getString("title")), true);
 				}
 				
 				channel.sendMessage(eb.build()).complete();
