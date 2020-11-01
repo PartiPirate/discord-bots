6f2cafdacd188ae9fb19f1dc03c7a20e1adbf943
Cédric Levieux <contact@levieuxcedric.com>
2020-11-01 11:26:20 +0100

    Another test

--- java-project/src/fr/partipirate/discord/bots/congressus/listeners/VocalChannelsHandler.java
+++ java-project/src/fr/partipirate/discord/bots/congressus/listeners/VocalChannelsHandler.java
@@ -1,5 +1,6 @@
 package fr.partipirate.discord.bots.congressus.listeners;
 
+import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -7,13 +8,21 @@ public class VocalChannelsHandler extends ListenerAdapter {
 
 	@Override
 	public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
+/*		
 		System.out.print("Member : ");
 		System.out.print(event.getMember().getNickname());
-		System.out.print("Event : ");
+		System.out.print(", Event : ");
 		System.out.print(event.getClass().getName());
-		System.out.print("Etat : ");
+		System.out.print(", Etat : ");
 		System.out.print(event.getVoiceState());
-		System.out.print("Channel : ");
+		System.out.print(", Channel : ");
 		System.out.println(event.getChannelLeft().getName());
+*/		
+	}
+	
+	@Override
+	public void onGenericEvent(Event event) {
+		System.out.print("Event : ");
+		System.out.println(event.getClass().getName());
 	}
 }
