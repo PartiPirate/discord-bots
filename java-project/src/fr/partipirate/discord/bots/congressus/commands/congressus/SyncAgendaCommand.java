a4959a25c908d9189f8e389d36a10b2c3caa037c
Cédric Levieux <contact@levieuxcedric.com>
2023-02-26 14:11:39 +0100

    Format bis

--- java-project/src/fr/partipirate/discord/bots/congressus/commands/congressus/SyncAgendaCommand.java
+++ java-project/src/fr/partipirate/discord/bots/congressus/commands/congressus/SyncAgendaCommand.java
@@ -6,29 +6,28 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 
-
 public class SyncAgendaCommand extends ACongressusCommand implements ICommand {
 
-	@Override
-	public String getKeyWord() {
-		return "agenda";
-	}
+    @Override
+    public String getKeyWord() {
+	return "agenda";
+    }
 
-	@Override
-	public boolean canDoCommand(User user, Guild guild) {
-		return true;
-	}
+    @Override
+    public boolean canDoCommand(User user, Guild guild) {
+	return true;
+    }
 
-	@Override
-	public void doCommand(User user, MessageChannel channel, Guild guild, String[] commandParts) {
-		String message = SyncAgenda.syncAgenda(guild);
-		if (message != null)
-			channel.sendMessage(message).complete();
+    @Override
+    public void doCommand(User user, MessageChannel channel, Guild guild, String[] commandParts) {
+	String message = SyncAgenda.syncAgenda(guild);
+	if (message != null) {
+	    channel.sendMessage(message).complete();
 	}
+    }
 
-	@Override
-	public String getCommandHelp() {
-		return "Synchronise l'agenda Discord avec l'agenda Congressus sur le prochain mois";
-	}
-
+    @Override
+    public String getCommandHelp() {
+	return "Synchronise l'agenda Discord avec l'agenda Congressus sur le prochain mois";
+    }
 }
